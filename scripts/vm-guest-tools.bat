4657005489e0907ad1a6c1bf99956bc0141a9a1a
Zdeněk Vydra <485514@mail.muni.cz>
2021-04-10 09:18:07 +0000

    Improve qemu

--- scripts/vm-guest-tools.bat
+++ scripts/vm-guest-tools.bat
@@ -28,7 +28,7 @@ goto :done
 
 :virtualbox
 
-move /Y C:\Users\vagrant\VBoxGuestAdditions.iso C:\Windows\Temp
+move /Y C:\Users\windows\VBoxGuestAdditions.iso C:\Windows\Temp
 cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\VBoxGuestAdditions.iso -oC:\Windows\Temp\virtualbox"
 cmd /c for %%i in (C:\Windows\Temp\virtualbox\cert\vbox*.cer) do C:\Windows\Temp\virtualbox\cert\VBoxCertUtil add-trusted-publisher %%i --root %%i
 cmd /c C:\Windows\Temp\virtualbox\VBoxWindowsAdditions.exe /S

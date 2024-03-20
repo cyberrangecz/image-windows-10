db4c576bda71e75d2d9e70529c379b8fb99e0c18
Zdeněk Vydra <485514@mail.muni.cz>
2024-03-20 12:43:18 +0100

    Fix certutil

--- scripts/spiceToolsInstall.ps1
+++ scripts/spiceToolsInstall.ps1
@@ -1,3 +1,3 @@
 
-certutil -AddStore "TrustedPublisher" "a:\redhat.cer"
+certutil -f -AddStore "TrustedPublisher" "a:\redhat.cer"
 Start-Process -FilePath "C:\Windows\Temp\spice-guest-tools.exe" -ArgumentList "/S" -Wait

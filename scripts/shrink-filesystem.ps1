69d6440f82c265352b6b54a7bfd7c02f79ce8c3a
Zdeněk Vydra <485514@mail.muni.cz>
2024-03-26 12:08:38 +0100

    Update

---
+++ scripts/shrink-filesystem.ps1
@@ -0,0 +1,13 @@
+# Get information about the system drive
+$partition = Get-Partition -DriveLetter 'C'
+
+# Defrag disk
+defrag C: /L /D /K /G /H
+defrag C: /X /H
+
+# Resize the system partition with the updated size
+@"
+sel disk $($partition.DiskNumber)
+sel part $($partition.PartitionNumber)
+shrink
+"@ | diskpart
